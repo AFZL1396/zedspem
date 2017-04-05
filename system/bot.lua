5e78f002a9a014f117f0421e0da6dd4face39655
AFZL1396 <abol.anvari@gmail.com>
2017-04-05 05:30:04 -0700

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -229,10 +229,19 @@ function create_config( )
     "supergroup",
     "language",
     "whitelist",
-    "tools"
+    "tools",
+"Add_Plugin",
+"Aparat",
+"Arz",
+"Azan",
+"Cpu",
+"rmsg",
+"Time",
+"Welcome",
+"Whitelist"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {(277313212)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
